e5d64b83fa1357a64ef4db7e9b570eb3731620a2
BrunoPan08 <brunopan08@gmail.com>
2024-05-10 12:04:04 -0300

    criando homepage

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 
 const Sidebar = () => (
@@ -16,13 +17,25 @@ const Sidebar = () => (
       <NavLink exact="true" activeclassname='active' to='/'>
         <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
       </NavLink>
-      <NavLink exact="true" activeclassname='active' to='/about'>
+      <NavLink exact="true" activeclassname='active' className="about-link" to='/about'>
         <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
       </NavLink>
       <NavLink exact="true" activeclassname='active' className="contact-link" to='/contact'>
         <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
       </NavLink>
     </nav>
+    <ul>
+      <li>
+        <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/bruno-pan-y-agua-machado-06561b15a/'>
+          <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e'/>
+        </a>
+      </li>
+      <li>
+        <a target='_blank' rel='noreferrer' href='https://github.com/BrunoPan08'>
+          <FontAwesomeIcon icon={faGithub} color='#4d4d4e'/>
+        </a>
+      </li>
+    </ul>
   </div>
 )
 
